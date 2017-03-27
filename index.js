65d8df4217df7d0baca7b0aaf8e0857ae978d71c
jiangjinjin <jjjisbest@sina.com>
2017-03-27 14:33:59 +0800

    第五次

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var all = require('./routes/all');
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 8080));
 
 app.use(express.static(__dirname + '/public'));
 
